5488add8bec48dcae1fd4e37394144e9c3204c10
bhagatprasad <bhagatprasad8@yahoo.com>
2026-02-04 09:03:11 +0530

    create city init

--- mytime-ui/src/app/admin/components/create-city.component.ts
+++ mytime-ui/src/app/admin/components/create-city.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { CountryService } from '../services/country.service';
+import { StateService } from '../services/state.service';
+import { State } from '../models/state';
+import { Country } from '../models/country';
+import { response } from 'express';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-create-city',
@@ -7,6 +13,33 @@ import { Component } from '@angular/core';
   templateUrl: './create-city.component.html',
   styleUrl: './create-city.component.css'
 })
-export class CreateCityComponent {
+export class CreateCityComponent implements OnInit, OnChanges {
 
+  states: State[] = [];
+
+  countries: Country[] = [];
+
+  constructor(private countryService: CountryService, private stateService: StateService) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+  ngOnInit(): void {
+    this.loadIntialData();
+  }
+
+  loadIntialData(): void {
+    forkJoin({
+      countries: this.countryService.getCountriesListAsync(),
+      states: this.stateService.getStateListAsync()
+    }).subscribe({
+      next: ({ countries, states }) => {
+        this.countries = countries;
+        this.states = states;
+      },
+      error: (error) => {
+        console.error('Error loading data:', error);
+      }
+    });
+  }
 }
