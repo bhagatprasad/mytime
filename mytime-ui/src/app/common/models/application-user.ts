dc1a2c35592a622b56a576d6194981df4f539ad3
bhagatprasad <bhagatprasad8@yahoo.com>
2026-02-05 21:10:40 +0530

    comit

--- mytime-ui/src/app/common/models/application-user.ts
+++ mytime-ui/src/app/common/models/application-user.ts
@@ -1,5 +1,5 @@
 export interface ApplicationUser {
-    id?: string;                   // Guid? in C#
+    id?: number;                   // Guid? in C#
     fullName?: string;             // string? in C#
     firstName?: string;            // string? in C#
     lastName?: string;             // string? in C#
