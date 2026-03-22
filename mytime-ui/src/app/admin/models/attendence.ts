44cc67ae51d2d451987db84cea9471c76053c146
soyamraghu <soyamraghu65@gmail.com>
2026-03-22 12:04:18 +0530

    commit

---
+++ mytime-ui/src/app/admin/models/attendence.ts
@@ -0,0 +1,30 @@
+export interface Attendence {
+  AttendenceId: number;
+
+  EmployeeId: number;
+  AttendenceDate: string;
+
+  CheckInTime: string;
+  CheckOutTime: string;
+
+  Status: string;
+  WorkHours: number;
+
+  Description?: string;
+
+  ApprovalStatus: string;
+
+  CreatedBy: number;
+  CreatedOn: string;
+
+  ModifiedBy?: number;
+  ModifiedOn?: string;
+
+  ApprovedBy?: number;
+  ApprovedOn?: string;
+
+  RejectedBy?: number;
+  RejectedOn?: string;
+
+  RejectionReason?: string;
+}
